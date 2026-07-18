6fc2cd29663901b2aa23193f622f43ce0fcd19dd
abhinavprakash-x <abhinavprakash.13x@gmail.com>
2026-07-18 17:53:08 +0000

    (update) optimised memcpy and memset

--- src/libc/mem.c
+++ src/libc/mem.c
@@ -8,15 +8,33 @@
 
 void *memset(void *dest, int val, size_t count)
 {
-    uint8_t *d = (uint8_t *)dest;
-    for(size_t i = 0; i < count; ++i) d[i] = (uint8_t)val;
+    size_t num_dwords = count / 4;
+    size_t remainder = count % 4;
+
+    uint32_t *d = (uint32_t *)dest;
+    uint32_t v = (uint8_t)val;
+
+    uint32_t fill = (v << 24) | (v << 16) | (v << 8) | v;
+    for(size_t i = 0; i < num_dwords; ++i) d[i] = fill;
+
+    uint8_t *d8 = (uint8_t *)(d + num_dwords);
+    for(size_t i = 0; i < remainder; ++i) d8[i] = (uint8_t)val;
+
     return dest;
 }
 
 void *memcpy(void *dest, const void *src, size_t count)
 {
-    uint8_t *d = (uint8_t *)dest;
-    const uint8_t *s = (uint8_t *)src;
-    for(size_t i = 0; i < count; ++i) d[i] = s[i];
+    size_t num_dwords = count / 4;
+    size_t remainder = count % 4;
+
+    uint32_t *d = (uint32_t *)dest;
+    const uint32_t *s = (uint32_t *)src;
+    for(size_t i = 0; i < num_dwords; ++i) d[i] = s[i];
+
+    uint8_t *d8 = (uint8_t *)(d + num_dwords);
+    const uint8_t *s8 = (const uint8_t *)(s + num_dwords);
+    for(size_t i = 0; i < remainder; ++i) d8[i] = s8[i];
+
     return dest;
 }
