db39a62faa012a9838f516891b44aac62832870b
abhinavprakash-x <abhinavprakash.13x@gmail.com>
2026-09-18 08:03:54 +0000

    bug_fix: BSS Section Filled with 0's for correct variable assignments

--- src/kernel_entry.asm
+++ src/kernel_entry.asm
@@ -1,9 +1,19 @@
 [bits 32]
 global _start
 
+extern __bss_start
+extern __bss_end
+extern kmain
+
 _start:
     ; Provide a clean entry point for the linker to hand off to C
-    extern kmain
+    mov edi, __bss_start
+    mov ecx, __bss_end
+    sub ecx, edi
+
+    xor eax, eax
+    rep stosb       ; Repeat while ecx != 0, [edi] = al, edi++ ecx--
+
     call kmain
 
 hang:
